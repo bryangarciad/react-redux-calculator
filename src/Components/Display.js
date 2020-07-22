1c94e2209d406461ce4a76cd4caecfca7bb0a799
bryangarciad <bryan.garcia.duran@gmail.com>
2020-07-21 22:05:00 -0600

    CssCorrections

--- src/Components/Display.js
+++ src/Components/Display.js
@@ -11,7 +11,7 @@ function Display(props){
             <section className="Wrapper">
                 <div className="CalcContainer" >
                     <div className="displayContainer">
-                        <h1 style={{textAlign:'right', height: '10px', fontSize:'1em', margin: '0px', paddingRight:"50px",fontWeight:"lighter"}}>{props.res}</h1>
+                        <h1 className="displayMain">{props.res}</h1>
                     </div>
                     <div className="keyboardContainer">
                         <KeyBoard></KeyBoard>
